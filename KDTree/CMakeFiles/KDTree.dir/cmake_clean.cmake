38d1b2a41db2faa03422694b8b89baa1f73648a5
JacobSh3n <91366199+JacobSh3n@users.noreply.github.com>
2021-11-05 17:32:55 +0800

    Add files via upload

---
+++ KDTree/CMakeFiles/KDTree.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KDTree.dir/kdmygod.cpp.o"
+  "CMakeFiles/KDTree.dir/kdmygod.cpp.o.d"
+  "KDTree"
+  "KDTree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KDTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
